062940706e76f0f5a19ea67ad0561cb1c3ce2216
jumaevkova04 <jumaevkovad@gmail.com>
2021-04-19 09:14:36 +0500

    add method ExportToFile

--- cmd/main.go
+++ cmd/main.go
@@ -1,45 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/jumaevkova04/wallet/pkg/wallet"
 )
 
 func main() {
-	svc := &wallet.Service{}
+	s := &wallet.Service{}
 
-	account, err := svc.RegisterAccount("+992000000001")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(account.ID)
-
-	account, err = svc.RegisterAccount("+992000000002")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(account.ID)
-
-	// err = svc.Deposit(account.ID, 10)
-	// if err != nil {
-	// 	switch err {
-	// 	case wallet.ErrAmountMustBePositive:
-	// 		fmt.Println("Сумма должно быть положительной")
-	// 	case wallet.ErrAccountNotFound:
-	// 		fmt.Println("Аккаунт пользователя не найден")
-	// 	}
-	// 	return
-	// }
-	// fmt.Println(account.Balance)
-
-	acc, err := svc.FindAccountByID(1)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(acc.ID)
+	s.RegisterAccount("+992000000001")
+	s.RegisterAccount("+992000000002")
+	s.RegisterAccount("+992000000003")
+	s.ExportToFile("dump.txt")
 
 }
